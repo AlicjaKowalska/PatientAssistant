ec199f3ab756633ec546b505e06ca142e13a2b6a
Piotr Gos <piotrgos45@gmail.com>
2022-12-09 13:35:25 +0100

    add insert to databse functionality to medicalAppointmentForm

--- app/src/main/java/com/example/patientassistant/Repository/AppointmentRepository.kt
+++ app/src/main/java/com/example/patientassistant/Repository/AppointmentRepository.kt
@@ -3,11 +3,14 @@ package com.example.patientassistant.Repository
 import androidx.annotation.WorkerThread
 import com.example.patientassistant.Model.Appointment
 import com.example.patientassistant.Room.AppointmentDAO
+import com.example.patientassistant.View.MedicalAppointment
 
 class AppointmentRepository (private val appointmentDAO: AppointmentDAO) {
 
     @WorkerThread //annotations can be done in a single thread
-    suspend fun insert(appointment : Appointment) {
+    suspend fun insert(appointment: Appointment) {
         appointmentDAO.insert(appointment)
     }
+
+
 }
